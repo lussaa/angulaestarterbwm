00a4cb4a5d035b0680fefcb242e2262c253f398c
lupaveli <lucija@pavelic.biz>
2019-11-03 09:13:33 +0100

    works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { HeaderComponent} from "./header/header.component";
 
 
 import { AppComponent } from './app.component';
+import { RentalComponent } from './rental/rental.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
     BrowserModule
